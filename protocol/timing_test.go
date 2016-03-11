1b8ecc3952d98def378ff3e71a9426f330b8cb57
spance <spance.wong@gmail.com>
2016-03-11 11:24:44 +0800

    fix test

--- protocol/timing_test.go
+++ protocol/timing_test.go
@@ -19,13 +19,14 @@ func Test_sleep(t *testing.T) {
 		}
 	}
 	for j := 0; j < len(intervals); j++ {
-		var exp, stdev float64
+		var exp, sum, stdev float64
 		exp = float64(intervals[j])
 		for _, v := range ret[j] {
+			sum += float64(v)
 			stdev += math.Pow(float64(v)-exp, 2)
 		}
 		stdev /= float64(loops)
 		stdev = math.Sqrt(stdev)
-		t.Logf("interval=%s sleeping=%s stdev=%.2f", time.Duration(intervals[j]), time.Duration(exp), stdev)
+		t.Logf("interval=%s sleeping=%s stdev/k=%.2f", time.Duration(intervals[j]), time.Duration(sum/loops), stdev/1e3)
 	}
 }
